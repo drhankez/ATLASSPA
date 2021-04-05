a9a0e23ddf476c6a63c2c0d87df6692a04054019
yacine hasnaoui <tebessa0atlas0yacine@outlook.com>
2021-04-05 10:17:44 +0200

    scrollbar fix ::::

--- ATLASSPA/Properties/Resources.Designer.cs
+++ ATLASSPA/Properties/Resources.Designer.cs
@@ -263,6 +263,16 @@ namespace ATLASSPA.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap loading_9 {
+            get {
+                object obj = ResourceManager.GetObject("loading-9", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap stunning_animated_gifs_01 {
             get {
                 object obj = ResourceManager.GetObject("stunning_animated_gifs_01", resourceCulture);
